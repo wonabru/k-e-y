23eeb3fddf42a419d759a4b908990929ca09bb7e
wonabru <admin@okuralabs.com>
2025-05-01 11:38:15 +0200

    OB-14 OB-15 check message and if fails ban IP

--- tcpip/listenerTcpService.go
+++ tcpip/listenerTcpService.go
@@ -283,7 +283,7 @@ func StartNewConnection(ip [4]byte, receiveChan chan []byte, topic [2]byte) {
 			for _, e := range rs[:len(rs)-1] {
 				if len(e) > 4 {
 					if bytes.Equal(e[:4], common.MessageInitialization[:]) {
-						receiveChan <- append(ip[:], e...)
+						receiveChan <- append(ip[:], e[4:]...)
 					} else {
 						BanIP(ip)
 						receiveChan <- []byte("EXIT")
