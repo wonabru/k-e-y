cb9ffc84d95dfdf7a427741dda0b02a235825cae
wonabru <admin@okuralabs.com>
2025-05-04 12:10:14 +0200

    OB-18 optimize number of blocks send in sync

--- tcpip/listenerTcpService.go
+++ tcpip/listenerTcpService.go
@@ -184,7 +184,7 @@ func StartNewConnection(ip [4]byte, receiveChan chan []byte, topic [2]byte) {
 			PeersMutex.Unlock()
 			if trust, ok := validPeersConnected[ip]; ok && trust <= 0 {
 				BanIP(ip)
-			} else {
+			} else if i == maxRetries-1 {
 				BanIP(ip)
 			}
 		}
